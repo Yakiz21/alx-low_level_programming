7d28164646c1c5fffdb914759b9e883e29350763
yakiz21 <yaqiss113@gmail.com.com>
2023-07-13 15:15:20 -0700

    size task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using the printf funtion
+ * Retun: 0 (Success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
+		
