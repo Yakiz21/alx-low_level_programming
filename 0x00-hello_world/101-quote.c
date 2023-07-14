e06b89276ac7bdbd22c7920cb43839f5e051e4a4
yakiz21 <yaqiss113@gmail.com.com>
2023-07-13 18:03:31 -0700

    quote task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
- * Return: always 1 (Success)
- */
+* main - A c program that prints a line to the standard error
+* Return: always 1 (Success)
+*/
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-1\n", stdout);
