dc29c25b6a0030367fcbc31f72aeca8aa991e367
yakiz21 <yaqiss113@gmail.com.com>
2023-07-19 06:27:52 -0700

    _putchar task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * main - Entry point of my code'
+ *
+ * Return: Aleays 0 (success)
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('u');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
