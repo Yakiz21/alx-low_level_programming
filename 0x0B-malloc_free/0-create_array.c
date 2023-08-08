094bd4406565140af4fbd8daf33089f682300cf0
yakiz21 <yaqiss113@gmail.com.com>
2023-08-08 12:06:11 -0700

    readme.md

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,7 +4,7 @@
  * create_array - create array of size size and assign char c
  * @size: size of array
  * @c: char to assign
- * Description: create array of size size and assign char c
+ * Description: creat array of size size and assign char c
  * Return: pointer to array, NULL if fail
  *
  */
